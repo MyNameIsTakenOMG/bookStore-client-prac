488f751404163da01c28a0ec15a56908d90a622f
Sean Fang <fangzhengonly@gmail.com>
2023-07-25 18:36:46 -0400

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import axios from 'axios';
+// import axios from 'axios';
+import { useEffect } from 'react';
 import './App.css';
 import baseURL from './config';
 
 function App() {
-  axios(`${baseURL}/api/v1/books`).then((response) => {
-    console.log(response.data);
-  });
+  // axios(`${baseURL}/api/v1/books`).then((response) => {
+  //   console.log(response.data);
+  // });
+  useEffect(() => {
+    fetch(`${baseURL}/api/v1/books`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        console.log(jsonResponse);
+      });
+
+    return () => {};
+  }, []);
 
   return <div className="App">my first component updated</div>;
 }
