e57a3effead10769f3f6b04633d7a64f5877a4fb
Sean Fang <fangzhengonly@gmail.com>
2023-07-25 18:25:07 -0400

    add config for multi-env

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import baseURL from './config';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  axios(`${baseURL}/api/v1/books`).then((response) => {
+    console.log(response.data);
+  });
+
+  return <div className="App">my first component updated</div>;
 }
 
 export default App;
