732758cccac3fca24716b6f40499814ba6d62b17
Sean Fang <fangzhengonly@gmail.com>
2023-07-27 14:37:51 -0400

    add unit tests to fetchBooksByTitle of bookSlice

--- src/redux-store/__tests__/book.test.js
+++ src/redux-store/__tests__/book.test.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import { store } from '../store';
-import { fetchAllBooks } from '../bookSlice';
+import { fetchAllBooks, fetchBooksByTitle } from '../bookSlice';
 
 jest.mock('axios');
 
 describe('book slice', () => {
-  it('should dispatch a successful action', async () => {
+  it('should successfully fetch books', async () => {
     axios.get.mockResolvedValue({
       data: [
         {
@@ -19,13 +19,39 @@ describe('book slice', () => {
     const reduxState = store.getState();
     expect(reduxState.entities.book.books.length).toEqual(1);
   });
-  it('should dispatch a failed action', async () => {
+
+  it('should fail to fetch books', async () => {
     axios.get.mockRejectedValue({
       message: 'error message',
       code: 'error code',
     });
     const result = await store.dispatch(fetchAllBooks());
     expect(result.type).toEqual('books/fetchAllBooks/rejected');
+    const reduxState = store.getState();
+    expect(reduxState.entities.book.error.message).toEqual('error message');
+  });
+
+  it('should successfully fetch books by title', async () => {
+    axios.get.mockResolvedValue({
+      data: [
+        {
+          title: 'test title',
+          description: 'test description',
+        },
+      ],
+    });
+    const result = await store.dispatch(fetchBooksByTitle('TEST TiTlE'));
+    expect(result.type).toEqual('books/fetchBooksByTitle/fulfilled');
+    const reduxState = store.getState();
+    expect(reduxState.entities.book.books.length).toEqual(1);
+  });
+  it('should fail to fetch books by title', async () => {
+    axios.get.mockRejectedValue({
+      message: 'error message',
+      code: 'error code',
+    });
+    const result = await store.dispatch(fetchBooksByTitle('TEST TiTlE'));
+    expect(result.type).toEqual('books/fetchBooksByTitle/rejected');
     const reduxState = store.getState();
     expect(reduxState.entities.book.error.message).toEqual('error message');
   });
--- src/redux-store/bookSlice.js
+++ src/redux-store/bookSlice.js
@@ -21,6 +21,29 @@ export const fetchAllBooks = createAsyncThunk(
   }
 );
 
+export const fetchBooksByTitle = createAsyncThunk(
+  'books/fetchBooksByTitle',
+  async (bookTitle, { rejectWithValue }) => {
+    try {
+      const response = await axios.get(`${baseUrl}/api/v1/books/${bookTitle}`);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue({
+        message: error.message,
+        code: error.code,
+      });
+    }
+  }
+);
+
+// matchers
+function booksPending(action) {
+  return action.type.startsWith('books') && action.type.endsWith('pending');
+}
+function booksRejected(action) {
+  return action.type.startsWith('books') && action.type.endsWith('rejected');
+}
+
 const bookSlice = createSlice({
   initialState: {
     books: [],
@@ -32,21 +55,29 @@ const bookSlice = createSlice({
     listBooks: (state) => {},
   },
   extraReducers: (builder) => {
-    builder.addCase(fetchAllBooks.pending, (state, action) => {
-      state.isLoading = true;
-    });
-    builder.addCase(fetchAllBooks.fulfilled, (state, action) => {
-      state.books = action.payload;
-      state.isLoading = false;
-    });
-    builder.addCase(fetchAllBooks.rejected, (state, action) => {
-      state.error = action.payload;
-      state.isLoading = false;
-    });
+    builder
+      .addCase(fetchAllBooks.fulfilled, (state, action) => {
+        state.books = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(fetchBooksByTitle.fulfilled, (state, action) => {
+        state.books = action.payload;
+        state.isLoading = false;
+      })
+      .addMatcher(booksPending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addMatcher(booksRejected, (state, action) => {
+        state.error = action.payload;
+        state.isLoading = false;
+      });
   },
 });
 
 export const { listBooks } = bookSlice.actions;
+export default bookSlice.reducer;
+
+// selectors
 
 export const bookListSelector = createSelector(
   (reduxState) => reduxState.entities.book,
@@ -61,5 +92,3 @@ export const bookListLoadingSelector = createSelector(
   (reduxState) => reduxState.entities.book,
   (book) => book.isLoading
 );
-
-export default bookSlice.reducer;
